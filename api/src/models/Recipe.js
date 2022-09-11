f5907cb0d97c659eff321997765e35cf32ec095d
Walter Celiz <wceliz.dev@gmail.com>
2022-09-10 21:08:36 -0300

    bd and relations

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -2,41 +2,49 @@ const { DataTypes } = require("sequelize");
 // Export a function that defines the model
 // then inject the connection to sequelize.
 module.exports = (sequelize) => {
-  sequelize.define("recipe", {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-      primaryKey: true,
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    summary: {
-      type: DataTypes.STRING(1000),
-      allowNull: false,
-    },
-    spoonacularScore: {
-      type: DataTypes.FLOAT(1),
-      validate: {
-        min: 0,
-        max: 100,
+  // define the model
+  sequelize.define(
+    "recipe",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      summary: {
+        type: DataTypes.STRING(1000),
+        allowNull: false,
+      },
+      spoonacularScore: {
+        type: DataTypes.FLOAT(1),
+        validate: {
+          min: 0,
+          max: 100,
+        },
+      },
+      healthScore: {
+        type: DataTypes.FLOAT(1),
+        allowNull: false,
+        validate: {
+          min: 0,
+          max: 100,
+        },
+      },
+      instructions: {
+        type: DataTypes.STRING,
+      },
+      image: {
+        type: DataTypes.STRING,
       },
     },
-    healthScore: {
-      type: DataTypes.FLOAT(1),
-      allowNull: false,
-      validate: {
-        min: 0,
-        max: 100,
-      },
-    },
-    instructions: {
-      type: DataTypes.STRING,
-    },
-    image: {
-      type: DataTypes.STRING,
-    },
-  });
+    {
+      timestamps: false,
+      createdAt: false,
+    }
+  );
 };
