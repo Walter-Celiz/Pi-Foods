46f57c809e5a10b13efa98a8887d086075a726d1
Walter Celiz <wceliz.dev@gmail.com>
2022-09-13 12:50:22 -0300

    reset client

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-import { BrowserRouter, Route, Switch } from "react-router-dom"
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <h1>Henry Food</h1>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <h1>Henry Food</h1>
+    </div>
   );
 }
 
--- client/src/reducer/index.js
+++
@@ -1,3 +0,0 @@
-function rootReducer() { }
-
-export default rootReducer;
