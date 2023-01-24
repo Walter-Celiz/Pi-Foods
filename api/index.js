0cdd5851cc804b26bbdc36a27f85ba8f78c1ccd7
Walter Celiz <wceliz.dev@gmail.com>
2023-01-24 08:45:18 -0300

    test deploy

--- api/index.js
+++ api/index.js
@@ -2,14 +2,14 @@ require("dotenv").config();
 const server = require("./src/app.js");
 const { db } = require("./src/db.js");
 const { PORT } = process.env;
-const { getAllDiets } = require("./src/controllers/dietController");
+// const { getAllDiets } = require("./src/controllers/dietController");
 
 // Syncing all the models at once.
 try {
   db.sync({ force: true }).then(() => {
-    getAllDiets();
-    console.log("Diets Charged 🟢🟢🟢🟢");
-    server.listen(3001, () => {
+    // getAllDiets();
+    // console.log("Diets Charged 🟢🟢🟢🟢");
+    server.listen(PORT, () => {
       console.log(`#Server listening at Port:${PORT}!!! 🟢🟢🟢🟢`);
     });
   });
